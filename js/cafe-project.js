4c1708f000d5830f6edb1365a32773aa8e71cfb6
diango27 <dim270684@gmail.com>
2026-07-02 16:56:46 +0200

    petite modificztion du html et de css, js terminé

--- js/cafe-project.js
+++ js/cafe-project.js
@@ -1,18 +1,18 @@
-// 1. On récupère les éléments HTML
-const btnDrink = document.getElementById("button-drink");
-const btnEat = document.getElementById("button-eat");
+// 1. Sélection des boutons et des div de contenu
+const btnEat = document.getElementById("btn-eat");
+const btnDrink = document.getElementById("btn-drink");
 
-const drink = document.getElementById("drink");
 const eat = document.getElementById("eat");
+const drink = document.getElementById("drink");
 
-// 2. Comportement au clic sur "Drink"
-btnDrink.addEventListener("click", () => {
-  // On cache toute la section nourriture (bouton + tableau)
-  eat.classList.add("not-visible");
+// 2. Clic sur le bouton "Eat"
+btnEat.addEventListener("click", () => {
+  eat.classList.remove("not-visible"); // On affiche la div #eat
+  drink.classList.add("not-visible"); // On cache la div #drink
 });
 
-// 3. Comportement au clic sur "Eat"
-btnEat.addEventListener("click", () => {
-  // On cache toute la section boissons (bouton + tableau)
-  drink.classList.add("not-visible");
+// 3. Clic sur le bouton "Drink"
+btnDrink.addEventListener("click", () => {
+  drink.classList.remove("not-visible"); // On affiche la div #drink
+  eat.classList.add("not-visible"); // On cache la div #eat
 });
